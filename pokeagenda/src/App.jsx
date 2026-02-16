a017be3a00047c18e647c9eff6f69babf84495fc
Javier <javninpen@alu.edu.gva.es>
2026-02-16 18:57:47 +0100

    Hito 1 Termiando

---
+++ pokeagenda/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Layout from './components/Layout.jsx';
+import Header from './components/Header.jsx';
+
+function App() {
+  return (
+    <div className="app">
+      <Layout>
+        <Header />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
